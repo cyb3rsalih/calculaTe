36fc11948cfb9db19f2cc70fcecee540b81eba64
Mehmet Salih Bindak <mehmetsalihbindak@gmail.com>
2019-06-10 10:55:44 +0300

    olmadı bastan

--- App.js
+++ App.js
@@ -33,18 +33,17 @@ export default class App extends Component {
 
 
   calculate = () => {
-    const { n1,mean1,stdDev1,stdDev2,n2,mean2 } = this.state
+
+    const { n1,n2,mean1,mean2,stdDev1,stdDev2 } = this.state    
 
     const abs = Math.abs(mean1 - mean2)
     const alt1 = Math.pow(stdDev1,2)/n1
     const alt2 = Math.pow(stdDev2,2)/n2
     const result = Math.sqrt(abs/(alt1+alt2))
-    alert(abs+' '+alt1+' '+alt2+' '+result)
     this.setState({result})
   }
  
   render() {
-    const { n1,mean1,stdDev1,stdDev2,n2,mean2,result } = this.state
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
         <View style={styles.container}>
@@ -73,38 +72,40 @@ export default class App extends Component {
         <View style={styles.kutu}>
           <Text>Sample 1</Text>
           <View style={styles.innerContainer}>
-            <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({n1})}} style={styles.textInput} value={n1} placeholder='N1 Value' >{n1}</TextInput>
+            <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({n1:parseFloat(this.state.n1)})}} style={styles.textInput} value={this.state.n1} placeholder='N1 Value' >{this.state.n1}</TextInput>
           </View>
 
           <View style={styles.innerContainer}>
-          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({mean1})}}  style={styles.textInput} value={mean1} placeholder='N1 Mean' >{mean1}</TextInput>
+          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({mean1:parseFloat(this.state.mean1)})}} style={styles.textInput} value={this.state.mean1} placeholder='N1 Mean' >{this.state.mean1}</TextInput>
           </View>
 
           <View style={styles.innerContainer}>
-          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({stdDev1})}}  style={styles.textInput} value={stdDev1} placeholder='N1 Std Dev.' >{stdDev1}</TextInput>
+          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({stdDev1:parseFloat(this.state.stdDev1)})}}  style={styles.textInput} value={this.state.stdDev1} placeholder='N1 Std Dev.' >{this.state.stdDev1}</TextInput>
+          </View>
+    
+        </View>
+      {/* Sample 2 Inputs */}
+        <View style={styles.kutu}>
+          <Text>Sample 2</Text>
+          <View style={styles.innerContainer}>
+            <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({n1:parseFloat(this.state.n2)})}} style={styles.textInput} value={this.state.n2} placeholder='N2 Value' >{this.state.n2}</TextInput>
+          </View>
+
+          <View style={styles.innerContainer}>
+          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({mean1:parseFloat(this.state.mean2)})}} style={styles.textInput} value={this.state.mean2} placeholder='N2 Mean' >{this.state.mean2}</TextInput>
+          </View>
+
+          <View style={styles.innerContainer}>
+          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({stdDev1:parseFloat(this.state.stdDev2)})}}  style={styles.textInput} value={this.state.stdDev2} placeholder='N2 Std Dev.' >{this.state.stdDev2}</TextInput>
           </View>
     
         </View>
 
-         {/* Sample 2 Inputs */}
-         <View style={styles.kutu}>
-         <Text>Sample 2</Text>
-          <View style={styles.innerContainer}>
-            <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({n2})}}  style={styles.textInput} value={n2} placeholder='N2 Value' >{n2}</TextInput>
-          </View>
+         
 
-          <View style={styles.innerContainer}>
-          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({mean2})}}  style={styles.textInput} value={mean2} placeholder='N2 Mean' >{mean2}</TextInput>
-          </View>
-
-          <View style={styles.innerContainer}>
-          <TextInput keyboardType='numeric' onSubmitEditing={Keyboard.dismiss} onChangeText={() => {this.calculate();this.setState({stdDev2})}}  style={styles.textInput} value={stdDev2} placeholder='N2 Std Dev.' >{stdDev2}</TextInput>
-          </View>
-    
-        </View>
         </View>
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}} >
-          <Text style={{fontSize:40,paddingBottom:100}}>Result : {result}</Text>
+          <Text style={{fontSize:40,paddingBottom:100}}>Result : {this.state.result}</Text>
         </View> 
         </View>
       </TouchableWithoutFeedback>
